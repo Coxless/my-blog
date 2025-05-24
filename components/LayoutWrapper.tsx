eb7d754d9c99be41128613dbe1c5277a60d10e6a
CoxlessSuperconductor <coxless.superconductor@gmail.com>
2025-05-24 18:56:50 +0000

    fix bugs

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,8 +1,8 @@
 import { Inter } from 'next/font/google'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
-import { ReactNode } from 'react'
-import Header from './Header'
+import type { ReactNode } from 'react'
+import { Header }  from './Header'
 
 interface Props {
   children: ReactNode
