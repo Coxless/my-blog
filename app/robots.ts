89b6b3acb1ccc166438a9b184ed2a146951c0560
CoxlessSuperconductor <coxless.superconductor@gmail.com>
2025-05-24 19:41:58 +0000

    bug fix

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,15 @@
-import { MetadataRoute } from 'next'
-import siteMetadata from '@/data/siteMetadata'
+import siteMetadata from "@/data/siteMetadata";
+import type { MetadataRoute } from "next";
 
-export const dynamic = 'force-static'
+export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
-    host: siteMetadata.siteUrl,
-  }
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+		},
+		sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+		host: siteMetadata.siteUrl,
+	};
 }
